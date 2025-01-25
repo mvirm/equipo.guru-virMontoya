c3e4a47ddd3dce543b7357ac395e82386a3aeda6
Virginia <mvirginiamontoya@yahoo.com.ar>
2025-01-25 16:00:42 -0300

    agrego comando en package.json

--- Api/src/controllers/subscriber/createSubscriber.js
+++ Api/src/controllers/subscriber/createSubscriber.js
@@ -1,42 +1,12 @@
 const {UserSubscriber} = require('../../db')
 
 const createSubscriber = async (email) => {
-    const[newSubscriber, created] = await UserSubscriber-findOrCreate({
+    const[newSubscriber, created] = await UserSubscriber.findOrCreate({
         where: {
             email
         }
     })
+    return newSubscriber; 
 }
 
 module.exports = {createSubscriber}
-
-const {Business} = require('../../db');
-
-    const createBusiness = async (name, password, address, city, country, email, phone, srcName, userId, contactId, socialMediaActiveId, msgReceivedId) => {
-        
-            const [newBusiness, created] = await Business.findOrCreate({
-                where: {
-                    name,
-                    password,
-                    address,
-                    city, 
-                    country, 
-                    email,
-                    phone,
-                    srcName,
-                },
-            })
-            
-            userId && await newBusiness.setUser(userId);
-            contactId && await newBusiness.addContact(contactId);
-            socialMediaActiveId && await newBusiness.addSocialMediaActive(socialMediaActiveId);
-            msgReceivedId && await newBusiness.setMsgReceived(msgReceivedId);
-
-            // console.log('newBusiness', newBusiness);
-    
-            return newBusiness;
-            
-    
-    };
-
-module.exports = {createBusiness};
