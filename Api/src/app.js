a2793560b4cda1acedd8668c271942bedd1fd3aa
Virginia <mvirginiamontoya@yahoo.com.ar>
2025-01-26 12:51:12 -0300

    ajusto politicas de cors

--- Api/src/app.js
+++ Api/src/app.js
@@ -16,13 +16,14 @@ server.use(express.json({ limit: '50mb' }));
 server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://mvirm.github.io/equipo.guru-virMontoya'], // Orígenes permitidos
+    origin: ['http://localhost:5173', 'https://mvirm.github.io'], // Orígenes permitidos
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
     credentials: true // Permite el uso de cookies en las solicitudes
 };
 
 server.use(cors(corsOptions));
+server.options('*', cors(corsOptions));
 server.use(cookieParser());
 server.use(morgan('dev')); 
 server.use('/', router);
